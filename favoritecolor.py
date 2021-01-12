1349a729ae1eceea2e9d7f3c161eed2ccff12b34
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-01-12 18:02:04 -0300

    first commit

---
+++ favoritecolor.py
@@ -0,0 +1,7 @@
+color = input("Please type your favorite color: ")
+print("Your favorite color is: " + color)
+animal = input("Please type your favorite animal? ")
+print("Your favorite animal is: " + animal)
+snack = input("Please type your favorite snack? ")
+print("Your favorite snack is: " + snack)
+print("\nYour " + color + " " + animal + " is eating your " + snack + "!")
