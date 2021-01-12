05f9072058cd90dec5457bbfd01acbf8d9b3bf17
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-01-12 19:54:37 -0300

    Second assignment added

---
+++ madLib.py
@@ -0,0 +1,29 @@
+print("Please enter the following:\n")
+
+#Inputs
+adjective = input("adjective: ")
+animal = input("animal: ")
+verbOne = input("verb: ")
+exclamation = input("exclamation: ")
+verbTwo = input("verb: ")
+verbThree = input("verb: ")
+
+#Logic a/an in last phrase
+article = "a"
+vowel = ['a', 'e', 'i', 'o', 'u']
+
+for letter in animal:
+    if letter in vowel:
+        article = "an"
+
+#Message
+print("\nYour story is:\n")
+
+print(f"The other day, I was really in trouble. It all started when I saw a very")
+print(f'{adjective} {animal} {verbOne} down the hallway. "{exclamation.capitalize()}!" I yelled. But all')
+print(f"I could think to do was to {verbTwo} over and over. Miraculously,")
+print(f"that caused it to stop, but not before it tried to {verbThree}")
+print("right in front of my family.\n")
+
+#Extra Message
+print(f"PS: {article} {animal} was used in that history")
