1331b024c785dbd574537b1bb33f041c84469d4e
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-02-15 22:07:41 -0300

    checkpoint w7

---
+++ w07Checkpoint.py
@@ -0,0 +1,15 @@
+number = 0
+candy = "no"
+
+while number < 0:
+    number= int(input("Please type a positive number: "))
+
+    if number > 0:
+        print(f"The number is: {number}")
+    else:
+        print("Sorry, that is a negative number. Please try again.")
+
+while candy != "yes"
+    candy - input("May I have a piece of candy? ")
+
+print("Thank You")
