255477f8fafacec63a75576509999b3266ca40af
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-02-22 10:15:37 -0300

    Instructor week 8 added

---
+++ instructorw08.py
@@ -0,0 +1,15 @@
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(f"The color is {color}")
+
+print()
+
+for number in range(1,9):
+    print(number)
+
+print()
+    
+for number in range (1,11):
+
+    print(f"{number * 2}")
