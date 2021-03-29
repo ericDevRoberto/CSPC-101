e98f7270199a9f3195fafbcd3ed47ddc7e1fe30c
Eric <ericsilva328@gmail.com>
2021-03-29 10:13:20 -0300

    checkpoint 13 done

---
+++ checkpointw13.py
@@ -0,0 +1,17 @@
+
+def display_regular(words):
+    print(words)
+
+def display_uppercase(words):
+    print(words.upper())
+
+def display_lowercase(words):
+    print(words.lower())
+
+name = input("What is your message? ")
+
+display_regular(name)
+
+display_uppercase(name)
+
+display_lowercase(name)
